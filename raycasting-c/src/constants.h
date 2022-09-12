b18086ed1884a6b0e5fbfae6dca9b416c2bc6337
Guilherme André Welter <guilherme.a.welter@gmail.com>
2022-09-12 08:38:30 -0300

    Displaying empty screen

---
+++ raycasting-c/src/constants.h
@@ -0,0 +1,5 @@
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+#define FALSE 0
+#define TRUE 1
--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -2,51 +2,81 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#include "constants.h"
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int isGameRunning = FALSE;
+
+int initializeWindow() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+  }
+  window = SDL_CreateWindow("Raycasting", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+                            SDL_WINDOW_BORDERLESS);
+  if (!window) {
+    fprintf(stderr, "Error criating window.\n");
+    return FALSE;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    fprintf(stderr, "Error criating renderer.\n");
+    return FALSE;
+  }
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+  return TRUE;
+}
+
+void destroyWindow() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
+void setup() {}
+
+void processInput() {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+  switch (event.type) {
+  case SDL_QUIT:
+    isGameRunning = FALSE;
+    break;
+  case SDL_KEYDOWN:
+    if (event.key.keysym.sym == SDLK_ESCAPE) {
+      isGameRunning = FALSE;
+    }
+    break;
+
+  default:
+    break;
+  }
+}
+
+void render() {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  // TODO:
+  // Render all game objects for this frame
+  SDL_RenderPresent(renderer);
+}
 
 int main(int argc, char **argv) {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("Error: SDL failed to initialize\nSDL Error: '%s'\n",
-           SDL_GetError());
-    return 1;
+  isGameRunning = initializeWindow();
+
+  setup();
+
+  while (isGameRunning) {
+    processInput();
+    // update();
+    render();
   }
 
-  SDL_Window *window =
-      SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED,
-                       SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-  if (!window) {
-    printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (!renderer) {
-    printf("Error: Failed to create renderer\nSDL Error: '%s'\n",
-           SDL_GetError());
-    return 1;
-  }
-
-  bool running = true;
-  while (running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-      case SDL_QUIT:
-        running = false;
-        break;
-
-      default:
-        break;
-      }
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-  }
+  destroyWindow();
 
   return 0;
 }
